43fe3bbc51b1c29e605593484978631c55a2302d
Jennifer Kang <jennikang@csu.fullerton.edu>
2017-02-27 20:52:07 -0800

    hi-jacked!

--- ottergram/scripts/hijack.js
+++ ottergram/scripts/hijack.js
@@ -1,6 +1,17 @@
-function silverchallenge() {
-  'use strict'
-  document.addEventListener('click', function(event) {
-    event.preventDefault();
-});
+function addLinkClickHandler() {
+    addEventListener('click', function(event) {
+        event.preventDefault();
+    });
 }
+
+function getLinks() {
+    var links = document.querySelectorAll('a');
+    return [].slice.call(links);
+}
+
+function initializeEvents() {
+    var link = getLinks();
+    link.forEach(addLinkClickHandler);
+}
+
+initializeEvents();
